da94e59ec84ef0dd272827b61faa2d2c0c1398c1
Jackcao1997 <qintao_cao@163.com>
2026-06-12 16:31:02 +0800

    feat: tl_description功能包中增加14个机械臂单独launch文件，同步增加Readme中的说明

---
+++ src/tl_description/launch/tcb605_description.launch.py
@@ -0,0 +1,28 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('tl_description')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim',
+            default_value='false',
+            description='Use joint_state_publisher_gui if true',
+            choices=['true', 'false'],
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_share, 'launch', 'tl_description.launch.py')
+            ),
+            launch_arguments={
+                'arm_type': 'tcb605',
+                'use_sim': LaunchConfiguration('use_sim'),
+            }.items(),
+        ),
+    ])
